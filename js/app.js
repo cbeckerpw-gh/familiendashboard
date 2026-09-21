35f3daee36b34b0d1ac4777a09a659248f7c7665
cbeckerpw-gh <cbecker.pw@googlemail.com>
2026-09-21 14:31:55 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // iCal URL des Google Kalenders
 const ICAL_URL = 'https://calendar.google.com/calendar/ical/family15160420290140632345%40group.calendar.google.com/public/basic.ics';
-// Wir nutzen CodeTabs als extrem stabilen CORS-Proxy
-const PROXY_URL = 'https://api.codetabs.com/v1/proxy?quest=' + encodeURIComponent(ICAL_URL);
+// Wir nutzen den stabilen Raw-Endpunkt von AllOrigins
+const PROXY_URL = 'https://api.allorigins.win/raw?url=' + encodeURIComponent(ICAL_URL);
 
 let realEvents = [];
 
