2de5f305840fe4d6ef55aea2c1956eef3bb4c662
cbeckerpw-gh <cbecker.pw@googlemail.com>
2026-09-25 11:09:29 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -155,7 +155,7 @@ function processLoadedEvents(events) {
 
             const formattedHours = String(hours).padStart(2, '0');
             const formattedMinutes = String(minutes).padStart(2, '0');
-            rawTime = `\({formattedHours}:\){formattedMinutes}`;
+            rawTime = `${formattedHours}:${formattedMinutes}`;
             timeStr = `${rawTime} Uhr`;
         }
 
